54aaa74cea0c3b3cda07759ca85a763dfec64dd1
Rey Garcia <reynaldogarcia@Reynaldos-iMac.attlocal.net>
2016-06-09 18:04:52 -0500

    val

--- js/app.js
+++ js/app.js
@@ -1,41 +1,56 @@
-$(document).ready(function() {
-    $('#submitButton').click(function(event) {
-        event.preventDefault();
-});
+//$(document).ready(function() {
 
-var questions = [
-    '1. Do ye like yer drinks strong?',
-    '2. Do ye like it with a salty tang?',
-    '3. Are ye a lubber who likes it bitter?',
-    '4. Would ye like a bit of sweetness with yer poison?',
-    '5. Are ye one for a fruity finish?',
-];
 
-var ingredients = {
-    strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
-    salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
-    bitter: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'],
-    sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
-    fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
-};
+function BartenderApp() {
 
- var generateQuestions = "";
-  for (var i = 0; i < questions.length; i++) {
-        $('#generateQuestions').append('<li id="questions">'+ (questions[i]) +'<br>' +'<select class="input"><option value="yes">Yes</option><option value="no">No</option></select>'+'</li>');
+    this.questions = [
+        '1. Do ye like yer drinks strong?',
+        // '2. Do ye like it with a salty tang?',
+        // '3. Are ye a lubber who likes it bitter?',
+        // '4. Would ye like a bit of sweetness with yer poison?',
+        // '5. Are ye one for a fruity finish?',
+    ];
+
+    this.ingredients = {
+        strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
+        salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
+        bitter: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'],
+        sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
+        fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
     }
+}
+BartenderApp.prototype.generateDrinks = function() {
+    for (var i = 0; i < this.questions.length; i++) {
+        $('#generateQuestions').append('<li id="questions">' + this.questions[i] + '<br>' + '</li>');
+    }
+    console.log(this.ingredients);
+}
 
 
 
- // function displayIngredients {   
-    // if (questions === yes){
-        // strong();
-        // salty();
-        // bitter();
-        // sweet();
-        // fruit();
-// }
+var app = new BartenderApp();
 
 
+$('#submitButton').click(function(event) {
+    event.preventDefault();
+    console.log('click working');
 
-
+    function getValue() {
+        $('#input').val();
+    }
+    console.log($('#input').val());
+    verifyData();
 });
+
+function verifyData(){
+if ($('#input').val() == 'yes'){
+    console.log(this.ingredients);
+}
+}
+
+
+app.generateDrinks();
+
+
+
+
