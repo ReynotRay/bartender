8df83469a77a22c4bd3d60413fd8051adde0ef7b
Rey Garcia <reynaldogarcia@Reynaldos-iMac.attlocal.net>
2016-06-13 01:05:28 -0500

    question1

--- js/app.js
+++ js/app.js
@@ -1,45 +1,26 @@
-// $(document).ready(main);
-//     $('#submitButton').click(function(event) {
-//     event.preventDefault();
-//     console.log('click working');
-//     console.log($('#input').val());
-//     app.verifyData();
-//     });
-
-function BartenderApp() {
-
-       this.currentQuestion = 0;
         
-        this.questions = [{
-        question:'1. Do ye like yer drinks strong?',
-        ingredients: ['Glug of rum', 'slug of whisky', 'splash of gin'],
-    }, {
-        quetsion:'2. Do ye like it with a salty tang?',
-        ingredients: ['Olive on a stick', 'salt-dusted rim','rasher of bacon'],
-    }, {
-        question:'3. Are ye a lubber who likes it bitter?',
-        ingredients: ['Shake of bitters', 'splash of tonic','twist of lemon peel'],
-    }, {
-        question:'4. Would ye like a bit of sweetness with yer poison?',
-        ingredients: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
-    }, {
-        question:'5. Are ye one for a fruity finish?',
-        ingredients: ['Slice of orange', 'dash of cassis', 'cherry on top']
-        }]; 
-     } 
-
-function main() {
-    console.log('what it dew');
-   // app.generateQuestions();
-
-}
-
-
-BartenderApp.prototype.generateQuestions = function() {
-        //$('#generateQuestions').text(questions[currentQuestion].question);
-        for (var i = 0; i < this.questions.length; i++) {
-            $('#generateQuestions').append('<li id="questions">' + this.questions[0] + '<br>' + '</li>');
+       function BartenderApp() { 
+        this.questions = ['1. Do ye like yer drinks strong?',
+            '2. Do ye like it with a salty tang?',
+            '3. Are ye a lubber who likes it bitter?',
+            // '4. Would ye like a bit of sweetness with yer poison?',
+            //  '5. Are ye one for a fruity finish?',
+         ];
+         this.ingredients = 
+             {
+                 strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
+                 salty: ['Olive on a stick', 'salt-dusted rim','rasher of bacon'],
+                 bitter: ['Shake of bitters', 'splash of tonic','twist of lemon peel'],
+                 sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
+                 fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
+            };
+         }
+ //create a loop that will append questions.
+BartenderApp.prototype.generateDrinks = function() {
+        for (var i = 0; i < this.questions.length;i++) {
         }
+        $('#generateQuestions').append('<li id="questions">' + this.questions[0] + '<br>' + '</li>');
+        console.log(this.questions[0]);
         //console.log(this.questions)
         //     if (currentQuestion === 0) {
         // for (var i = 0; i < questions.length; i++) {
@@ -49,7 +30,6 @@ BartenderApp.prototype.generateQuestions = function() {
         //             }
     }
         
-this.quetsions
 
 BartenderApp.prototype.verifyData = function () {
     if ($('#input').val() == 'yes') {
@@ -65,15 +45,15 @@ BartenderApp.prototype.verifyData = function () {
     }
 }
 
-BartenderApp.prototype.displayIngredients = function() {
-    console.log(this.questions);
-$(this.questions).show();
-    console.log('what the fucks');
-$.each(this.questions, function (key,value){
-    $('#generateQuestions').append('<p>' + key + ' : ' + value  + '</li>');
-});
-
-}
+ BartenderApp.prototype.displayIngredients = function() {
+         console.log(this.ingredients);
+         $('.ingredients').show();
+         
+ $.each(this.ingredients, function (key,value){
+     $('.ingredients').append('<p>' + key + ':' + value +'</li>');
+  });
+  
+  }
 //run the function generate drinks
 
 
@@ -91,7 +71,7 @@ var app = new BartenderApp();
 
 });
 
-app.generateQuestions();
+app.generateDrinks();
 
 
 
