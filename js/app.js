24fa193cbb58ab228b9753ed5b4ebfec56fec038
Rey Garcia <reynaldogarcia@Reynaldos-iMac.attlocal.net>
2016-06-15 00:45:19 -0500

    edit

--- js/app.js
+++ js/app.js
@@ -8,74 +8,72 @@ function BartenderApp() {
         '5. Are ye one for a fruity finish?',
     ];
     this.ingredients = {
-        strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
-        salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
-        bitter: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'],
-        sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
-        fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
+        strong: ['Glug of rum,', ' slug of whisky,', ' & splash of gin.'],
+        salty: ['Olive on a stick','salt-dusted rim', ' & rasher of bacon.'],
+        bitter: ['Shake of bitters','splash of tonic', ' & twist of lemon peel.'],
+        sweet: ['Sugar cube','spoonful of honey', ' & splash of cola.'],
+        fruity: ['Slice of orange','dash of cassis', ' & cherry on top.']
     };
 }
 BartenderApp.prototype.displayFirstQuestion = function() {
     $('#generateQuestions').append('<li>'+this.questions[0]+'</li>');
-
 };
 
 
-// BartenderApp.protype.nextQuestion = fuction(){
-$( "#next" ).click(function() {
-  app.displaySecondQuestion();
-});
-
+BartenderApp.prototype.nextQuestion = function(){
+$("#next").click(function() {
+    console.log('click');
+ console.log(this.questions);
+    $('.ingredients').remove();
+    app.displaySecondQuestion();
+    }); 
+//}
 BartenderApp.prototype.displaySecondQuestion = function (){
-     console.log("before");
+     // console.log("before");
+    
     $('#generateQuestions').append('done');
-    console.log("after");
+    //console.log("after");
     $('#generateQuestions').append('<li>'+this.questions[1]+'</li>');
+    $('#generateQuestions:first-of-type').remove();
    
 };
 
+
 BartenderApp.prototype.verifyData = function() {
-    if ($('#input').val() == 'yes' || this.questions == [0]) {
-        console.log('maybe got it');
+    if ($('#input').val() == 'yes' && this.questions[0]) {
+        console.log('first question correct');
         app.displayIngredients();
-        $('#submitButton').hide();
+        // $('#submitButton').hide();
         $('.maybe').hide();
         $('#next').show();
-    } else if ($('#input').val() == 'yes'|| this.questions == [1]) {
-        app.displayIngredients();
-        $('#submitButton').hide();
-        $('.maybe').hide();
-        $('#next').show();
-    } else if ($('#input').val() == 'yes'|| this.questions == [2]) {
-        app.displayIngredients();
-        $('#submitButton').hide();
-        $('.maybe').hide();
-        $('#next').show();
+         } 
+    else if ($('#input').val() == 'yes' && this.questions[1]) {
+        console.log('second final question');
+        // app.displayIngredients();
+        // $('#submitButton').hide();
+        // $('.maybe').hide();
+        // $('#next').show();
+    } else if ($('#input').val() == 'yes' && this.questions[2]) {
+        console.log('3 final question');
+
     }
-    else if ($('#input').val() == 'yes'|| this.questions == [3]) {
-        app.displayIngredients();
-        $('#submitButton').hide();
-        $('.maybe').hide();
-        $('#next').show();
+    else if ($('#input').val() == 'yes'|| this.questions[3]) {
+        console.log('4 final question');
+
     }
-    else if ($('#input').val() == 'yes'|| this.questions == [4]) {
-        app.displayIngredients();
-        $('#submitButton').hide();
-        $('.maybe').hide();
-        $('#next').show();
+    else if ($('#input').val() == 'yes'|| this.questions[4]) {
+        console.log('5 final question');
     }
-    else{
-        consoel.log('error');
+    else{   
+        console.log('finised');
+        $('#pirate').show();
     }
 };
 
 BartenderApp.prototype.displayIngredients = function() {
-    console.log(this.ingredients);
     $('.ingredients').show();
-
-    $.each(this.ingredients, function(key, value) {
-        $('.ingredients').append('<p>' + key + ':' + value + '</li>');
-    });
+        $('.ingredients').append(this.ingredients["strong"]);
+    //});
 
 };
 //run the function generate drinks
@@ -88,9 +86,6 @@ var app = new BartenderApp();
 
 $('#submitButton').click(function(event) {
     event.preventDefault();
-    console.log('click working');
-
-    console.log($('#input').val());
     app.verifyData();
 
 });
