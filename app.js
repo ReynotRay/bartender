203baf0654230c2222b84b3e9dd6811dda01ff90
ReynotRay <reygarcia718@gmail.com>
2016-06-18 13:13:47 -0500

    questions working

--- app.js
+++ app.js
@@ -9,34 +9,25 @@
         'Are ye one for a fruity finish?',];
  
 
-    this.ingredients = {
+    var ingredients = {
         strong: ['Glug of rum', ' slug of whisky', ' & splash of gin.'],
         salty: ['Olive on a stick', 'salt-dusted rim',' & rasher of bacon.'],
         bitter: ['Shake of bitters', 'splash of tonic',' & twist of lemon peel.'],
         sweet: ['Sugar cube', 'spoonful of honey', ' & splash of cola.'],
         fruity: ['Slice of orange', 'dash of cassis',' & cherry on top.'
         ]
-}
+};
 function generateQuestions(){
     console.log(questions);
+    
  for (var i = 0; i < questions.length; i++) {
-    generateQuestions += questions[i] + '<br>';     
-     }
-     $('.generateQuestionsList').append("<div>"generateQuestions"</div");
-    console.log("fucks");
-        
-    }
+    //generateQuestions += questions[i] + '<br>';     
+     $('.generateQuestionsList').append(questions[i] +'<br>' +'<input name="check" value="true"' + '</input>' + '<br>');
+        console.log("fucks");   
+    
+}
+}
 
-
-
-
-
-
-
-
-
-
-
-       // var app = new BartenderApp();
+    // var app = new BartenderApp();
     //start app with displayQuetsions
     generateQuestions();
