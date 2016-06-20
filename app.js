e902321d6e752d3604033c4a80bb57059147a1a4
ReynotRay <reygarcia718@gmail.com>
2016-06-20 18:04:47 -0500

    if statement working

--- app.js
+++ app.js
@@ -17,17 +17,71 @@
         fruity: ['Slice of orange', 'dash of cassis',' & cherry on top.'
         ]
 };
+
+// var drink = function(ingredients) {
+//     console.log(ingredients);
+//     this.strong = ingredients.strong;
+//     this.salty = ingredients.salty;
+//     this.bitter = ingredients.bitter;
+// };
+
+//working random number
+//     var ingredients = 
+//       ['Glug of rum','slug of whisky', 'splash of gin']
+ 
+//     ;
+
+// var randomElementFromArray = Math.floor(Math.random() * ingredients.length);
+
+// console.log(ingredients[randomElementFromArray]);
+
+
+// var randomElementFromArray = function(){
+//     var newNumber = Math.floor(Math.random() + ingredients.length);
+//     console.log(ingredients[randomElementFromArray]);
+// };
+
+
 function generateQuestions(){
-    console.log(questions);
     
- for (var i = 0; i < questions.length; i++) {
-    //generateQuestions += questions[i] + '<br>';     
-     $('.generateQuestionsList').append(questions[i] +'<br>' +'<input name="check" value="true"' + '</input>' + '<br>');
-        console.log("fucks");   
+ for (var i = 0; i < questions.length; i++) { 
+     $('.generateQuestionsList').append('<div>'+'<p>' + questions[i] +'<br> yes <input name=check' + i +
+    ' value="true" type=radio <br> No! <input name=check' + i + ' type=radio value="false"></p>'+'</div>');
+    }
     
-}
+//function submit(){
+    $('#submitAnswers').click(function(event){
+    event.preventDefault();
+    //retrieve the selection
+    var userChoice = $('form').serializeArray();
+       if (userChoice[0].value === "true") {
+    console.log("inside0");
+        }
+    //     else if (userChoice[1].value === "true") {
+    // console.log("inside1");
+    //     }
+    //     else if (userChoice[2].value === "true") {
+    // console.log("inside2");
+    //     }
+    //     else if (userChoice[3].value === "true") {
+    // console.log("inside3");
+    //     }
+    //     else if (userChoice[4].value === "true") {
+    // console.log("inside4");
+    //     }
+    //     else{
+    //         console.log('nothing');
+    //     }
+        
+    console.log(userChoice);
+    //checkTrue();
+    
+    });
+
+
 }
 
     // var app = new BartenderApp();
     //start app with displayQuetsions
     generateQuestions();
+   //random element from array randomElementFromArray();
--- readme.md
+++ readme.md
@@ -1,6 +1,19 @@
 Bartender APP
 If you like to add a spice to your drink then this game is for you.
 
+
+
+when input fields are clicked on then submited console log they are clicked
+
+random number next 
+
+make suer working
+
+display ingredients
+
+when clicked on yes, display correct answers.
+
+<!-- 
 Math random working 
 
 look through questions
@@ -9,4 +22,4 @@ display ingreiends with random
 
 input has to be false. 
 
-when true then display cancaushin 
+when true then display cancaushin  -->
