0e62827a6614743cd675b556c6312317107b8b5b
Rey Garcia <reynaldogarcia@Reynaldos-iMac.attlocal.net>
2016-06-22 23:19:18 -0500

    edit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function BartenderApp(){ 
+
+     var questions = function(questions) {
+        this.questions = question;
+    };
+     questions =   ['Do ye like yer drinks strong?',
+        'Do ye like it with a salty tang?',
+        'Are ye a lubber who likes it bitter?',
+        'Would ye like a bit of sweetness with yer poison?',
+        'Are ye one for a fruity finish?',];
+ 
+     var ingredients = function(ingredients) {
+        this.ingredients = ingredients;
+    };
+     var ingredients = {
+        strong: ['glug of rum', ' slug of whisky', ' splash of gin'],
+        salty: [' olive on a stick', ' salt-dusted rim',' rasher of bacon'],
+        bitter: [' shake of bitters', ' splash of tonic',' twist of lemon peel'],
+        sweet: [' sugar cube', ' spoonful of honey', ' splash of cola'],
+        fruity: [' slice of orange', ' dash of cassis',' cherry on top']
+};
