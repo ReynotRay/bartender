941b9bc742562bf277b377f70a901780f01ccc21
ReynotRay <reygarcia718@gmail.com>
2016-06-27 17:51:10 -0500

    edit

--- js/fucks.js
+++ js/fucks.js
@@ -1,8 +1,4 @@
-function BartenderApp(){ 
-//questions constructor
-var questions = function(question) {
-    this.question = question;
-};
+
 
 var strong = new questions(['Do ye like yer drinks strong?']);
 var salty = new questions(['Do ye like it with a salty tang?']);
@@ -10,22 +6,30 @@ var bitter = new questions(['Are ye a lubber who likes it bitter?']);
 var sweet = new questions(['Would ye like a bit of sweetness with yer poison?']);
 var fruity = new questions(['Are ye one for a fruity finish?']);
 
-console.log(strong.question);
-
-}
-
-//ingreidnets constructor
-var ingredients = function(ingredient) {
-    this.ingredient = ingredient;
-};
 var strong = new ingredients(['glug of rum', ' slug of whisky', ' splash of gin']);
 var salty = new ingredients([' olive on a stick', ' salt-dusted rim',' rasher of bacon']);
 var bitter = new ingredients([' shake of bitters', ' splash of tonic',' twist of lemon peel']);
 var sweet = new ingredients([' sugar cube', ' spoonful of honey', ' splash of cola']);
 var fruity = new ingredients([' slice of orange', ' dash of cassis',' cherry on top']);
 
-console.log(strong.ingredient);
 
-//create constructor for pantry 
+var preferences = {};
 
-///create drink method
+if(strong){
+	preferences.strong = true;
+}
+if(sweet){
+	preferences.sweet = true;
+}
+
+preference = {
+	strong:true,
+	sweet:true
+};
+
+Bartender.prototype.createDrink = function (preferences){
+	var drink = ['the fucks drink'];
+	if (preferences.strong){
+		drink.push(preferences.strong[randomnumber]);
+	}
+};
